4ad46c7c9a3d26e287bc744d17f22cbfd319ca25
Georgia Tzelepi <georgiatzgr@gmail.com>
2021-12-05 15:14:22 +0200

    lab7

---
+++ week7/lab7/5.sql
@@ -0,0 +1,2 @@
+-- return the average energy of all the songs
+SELECT AVG(energy) FROM songs;
---
+++ week7/lab7/8.sql
@@ -0,0 +1,2 @@
+-- list the names of the songs that feature other artists
+SELECT name FROM songs WHERE name LIKE "%feat.%";
